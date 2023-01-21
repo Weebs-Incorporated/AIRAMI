930290794c0f152b2167fefc31fcdd9f65e160f9
NachoToast <32235595+NachoToast@users.noreply.github.com>
2023-01-21 13:20:49 +1300

    remove `InternalLink` margin

--- src/components/Links/InternalLink.tsx
+++ src/components/Links/InternalLink.tsx
@@ -1,5 +1,5 @@
 import { Link, LinkProps } from 'react-router-dom';
 
 export const InternalLink = (props: LinkProps) => (
-    <Link {...props} style={{ color: 'inherit', textDecoration: 'inherit', margin: 0, ...props.style }} />
+    <Link {...props} style={{ color: 'inherit', textDecoration: 'inherit', ...props.style }} />
 );
