775267d709311a58ff1551dcafc01f9094768436
NachoToast <32235595+NachoToast@users.noreply.github.com>
2023-01-22 16:52:36 +1300

    add users page

---
+++ src/pages/Users/UserRow.tsx
@@ -0,0 +1,106 @@
+import { useMemo, useState } from 'react';
+import { TableRow, TableCell, Typography, Button, Link } from '@mui/material';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import { UserBadges } from '../../components/Icons';
+import PermissionEditor from '../../components/PermissionEditor/PermissionEditor';
+import ProfilePicture from '../../components/ProfilePicture/ProfilePicture';
+import { UserSession } from '../../contexts';
+import { hasPermission } from '../../helpers';
+import { AIMS } from '../../types';
+
+import EditIcon from '@mui/icons-material/Edit';
+import { InternalLink } from '../../components/Links';
+
+dayjs.extend(relativeTime);
+
+export interface UserRowProps {
+    user: AIMS.ClientFacingUser;
+    showIp: boolean;
+    showFullPermissions: boolean;
+    loggedInUser: UserSession;
+    onPermissionUpdate: (newPermissions: AIMS.UserPermissions, isSelf: boolean) => void;
+}
+
+const UserRow = (props: UserRowProps) => {
+    const { user, showIp, showFullPermissions, loggedInUser, onPermissionUpdate } = props;
+
+    const [permissionElementOpen, setPermissionElementOpen] = useState(false);
+
+    const isSelf = useMemo(() => user._id === loggedInUser.userData._id, [loggedInUser.userData._id, user._id]);
+
+    const permissionElement = useMemo(() => {
+        if (
+            !hasPermission(loggedInUser.userData, AIMS.UserPermissions.Owner) &&
+            hasPermission(user, AIMS.UserPermissions.Owner)
+        ) {
+            return <></>;
+        }
+
+        return (
+            <>
+                <Button
+                    variant="outlined"
+                    color="info"
+                    startIcon={<EditIcon />}
+                    onClick={(e) => {
+                        e.preventDefault();
+                        setPermissionElementOpen(true);
+                    }}
+                    size="small"
+                >
+                    Edit
+                </Button>
+                <PermissionEditor
+                    loggedInUser={loggedInUser}
+                    open={permissionElementOpen}
+                    onClose={() => {
+                        setPermissionElementOpen(false);
+                    }}
+                    onPermissionsUpdate={(newPermissions) => onPermissionUpdate(newPermissions, isSelf)}
+                    targetUser={user}
+                />
+            </>
+        );
+    }, [isSelf, loggedInUser, onPermissionUpdate, permissionElementOpen, user]);
+
+    return (
+        <TableRow hover>
+            <TableCell>
+                <InternalLink to={`/users/${user._id}`}>
+                    <Typography sx={{ display: 'flex', alignItems: 'center' }}>
+                        <ProfilePicture user={user} size={24} />
+                        &nbsp;
+                        <Link underline="hover" component="span">
+                            {user.username}#{user.discriminator}
+                        </Link>
+                    </Typography>
+                </InternalLink>
+            </TableCell>
+            <TableCell>{user.posts}</TableCell>
+            {showIp && <TableCell>{user.latestIp}</TableCell>}
+            <TableCell>
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <UserBadges user={user} showAll={showFullPermissions} justifyContent="flex-start" />
+                    {permissionElement}
+                </div>
+            </TableCell>
+            <TableCell title={new Date(user.registered).toUTCString()}>
+                <Typography>
+                    {new Date(user.registered).toLocaleDateString('en-NZ')}
+                    <br />
+                    <span style={{ color: 'gray' }}>{dayjs(user.registered).fromNow()}</span>
+                </Typography>
+            </TableCell>
+            <TableCell title={new Date(user.lastLoginOrRefresh).toUTCString()}>
+                <Typography>
+                    {new Date(user.lastLoginOrRefresh).toLocaleDateString('en-NZ')}
+                    <br />
+                    <span style={{ color: 'gray' }}>{dayjs(user.lastLoginOrRefresh).fromNow()}</span>
+                </Typography>
+            </TableCell>
+        </TableRow>
+    );
+};
+
+export default UserRow;
--- src/pages/Users/UsersPage.tsx
+++ src/pages/Users/UsersPage.tsx
@@ -1,14 +1,246 @@
-import { Typography } from '@mui/material';
+import {
+    Button,
+    Collapse,
+    LinearProgress,
+    Stack,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TablePagination,
+    TableRow,
+    Typography,
+} from '@mui/material';
+import { useContext, useEffect, useMemo, useState } from 'react';
+import { aims } from '../../api';
+import { HomeButton } from '../../components/Buttons';
 import Footer from '../../components/Footer';
+import SiteBreadcrumbs from '../../components/SiteBreadcrumbs/SiteBreadcrumbs';
+import { SettingsContext, UserSession, UserSessionContext } from '../../contexts';
+import { hasPermission } from '../../helpers';
+import { AIMS } from '../../types';
+import { Page } from '../Page.styled';
 
-const UsersPage = () => {
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import UserRow from './UserRow';
+export interface UsersPageProps {
+    loggedInUser: UserSession;
+}
+
+const UsersPage = ({ loggedInUser }: UsersPageProps) => {
+    const { settings } = useContext(SettingsContext);
+    const { controllers: userControllers } = useContext(UserSessionContext);
+
+    const [page, setPage] = useState(0);
+    const [perPage, setPerPage] = useState(20);
+    const [totalUserCount, setTotalUserCount] = useState(0);
+    const [error, setError] = useState('');
+    const [users, setUsers] = useState<AIMS.ClientFacingUser[]>();
+    const [isRevealingIps, setIsRevealingIps] = useState(false);
+    const [isShowingAllPermissions, setIsShowingAllPermissions] = useState(false);
+
+    const canShowIps = useMemo(
+        () => hasPermission(loggedInUser.userData, AIMS.UserPermissions.Owner),
+        [loggedInUser.userData],
+    );
+
+    useEffect(() => {
+        const controller = new AbortController();
+
+        aims.getUsers(
+            {
+                baseURL: settings.serverUrl,
+                siteToken: loggedInUser.siteToken,
+                controller,
+                rateLimitBypassToken: settings.rateLimitBypassToken,
+            },
+            { page, perPage },
+            null,
+        ).then((res) => {
+            if (res === 'canceled') {
+                setError('User fetching was aborted.');
+            } else if (res.success) {
+                setUsers(res.data.users);
+                setTotalUserCount(res.data.pagination.itemCount);
+                setError('');
+            } else if (res.generic) {
+                setError(`Error ${res.status}${res.statusText !== '' ? `: ${res.statusText}` : ''}`);
+            } else {
+                switch (res.status) {
+                    case 401:
+                        setError(`Error 401: ${res.data}`);
+                        break;
+                    case 403:
+                        setError('Missing permissions to fetch users.');
+                        break;
+                    case 404:
+                        setError('Fetching failed, you account may have been deleted.');
+                        break;
+                    case 429:
+                        setError(`Rate limited, try again in ${res.data.reset} seconds.`);
+                        break;
+                    case 501:
+                        setError('User database is not enabled.');
+                        break;
+                    default:
+                        throw res;
+                }
+            }
+        });
+
+        return () => {
+            controller.abort();
+        };
+    }, [page, perPage, settings.rateLimitBypassToken, settings.serverUrl, loggedInUser.siteToken]);
+
+    const paginationElement = useMemo(() => {
+        if (users === undefined) return <></>;
+
+        return (
+            <TablePagination
+                component="div"
+                sx={{ alignSelf: 'flex-start' }}
+                labelRowsPerPage="Users per page"
+                rowsPerPageOptions={[20, 50, 100]}
+                count={totalUserCount}
+                rowsPerPage={perPage}
+                page={page}
+                onPageChange={(e, newPage) => {
+                    e?.preventDefault();
+                    setPage(newPage);
+                }}
+                onRowsPerPageChange={(e) => {
+                    e.preventDefault();
+                    setPerPage(parseInt(e.target.value, 10));
+                    setPage(0);
+                }}
+            />
+        );
+    }, [page, perPage, totalUserCount, users]);
+
     return (
         <>
-            <div style={{ flexGrow: 1 }} />
-            <Typography variant="h3">Coming Soon</Typography>
-            <Footer />
+            {error !== '' && (
+                <Collapse in>
+                    <Typography color="lightcoral">{error}</Typography>
+                </Collapse>
+            )}
+            <Stack direction="row" spacing={1} justifyContent="flex-end" sx={{ width: '100%' }}>
+                <Button
+                    variant="outlined"
+                    onClick={(e) => {
+                        e.preventDefault();
+                        setIsShowingAllPermissions(!isShowingAllPermissions);
+                    }}
+                >
+                    {isShowingAllPermissions ? 'Partial' : 'All'} Permissions
+                </Button>
+                {canShowIps && (
+                    <Button
+                        variant="outlined"
+                        onClick={(e) => {
+                            e.preventDefault();
+                            setIsRevealingIps(!isRevealingIps);
+                        }}
+                        startIcon={isRevealingIps ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                    >
+                        {isRevealingIps ? 'Hide' : 'Reveal'} IPs
+                    </Button>
+                )}
+            </Stack>
+            {paginationElement}
+            <TableContainer>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>User</TableCell>
+                            <TableCell>Posts</TableCell>
+                            {isRevealingIps && <TableCell>IP</TableCell>}
+                            <TableCell>Permissions</TableCell>
+                            <TableCell>Registered</TableCell>
+                            <TableCell>Last Seen</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {users === undefined ? (
+                            <TableRow>
+                                <TableCell colSpan={6}>
+                                    <LinearProgress />
+                                </TableCell>
+                            </TableRow>
+                        ) : (
+                            <>
+                                {users.map((e) => (
+                                    <UserRow
+                                        key={e._id}
+                                        user={e}
+                                        showFullPermissions={isShowingAllPermissions}
+                                        showIp={isRevealingIps}
+                                        loggedInUser={loggedInUser}
+                                        onPermissionUpdate={(newPermissions, isSelf) => {
+                                            const userIndex = users.indexOf(e);
+                                            users[userIndex] = { ...users[userIndex], permissions: newPermissions };
+
+                                            setUsers([...users]);
+                                            if (isSelf) {
+                                                userControllers.updatePermissions(newPermissions);
+                                            }
+                                        }}
+                                    />
+                                ))}
+                            </>
+                        )}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            {paginationElement}
         </>
     );
 };
 
-export default UsersPage;
+const UsersPageWrapper = () => {
+    const { user } = useContext(UserSessionContext);
+
+    if (user === null) {
+        return (
+            <>
+                <SiteBreadcrumbs
+                    items={[
+                        { to: '/', text: 'Home' },
+                        { to: '/users', text: 'Users' },
+                    ]}
+                />
+                <Page>
+                    <div style={{ flexGrow: 1 }} />
+                    <Typography variant="h3" gutterBottom>
+                        Not Cool Enough
+                    </Typography>
+                    <Typography color="gray" textAlign="center">
+                        Regular users are not allowed to access this page.
+                    </Typography>
+                    <HomeButton sx={{ mt: 3 }} />
+                    <Footer />
+                </Page>
+            </>
+        );
+    }
+
+    return (
+        <>
+            <SiteBreadcrumbs
+                items={[
+                    { to: '/', text: 'Home' },
+                    { to: '/users', text: 'Users' },
+                ]}
+            />
+            <Page maxWidth="xl">
+                <UsersPage loggedInUser={user} />
+                <Footer />
+            </Page>
+        </>
+    );
+};
+
+export default UsersPageWrapper;
